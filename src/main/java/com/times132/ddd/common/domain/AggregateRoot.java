c329de2570154262c2efc15727693c29cac22991
times132 <times132@gmail.com>
2022-11-01 10:34:52 +0900

    refactoring AggregateRoot

--- src/main/java/com/times132/ddd/common/domain/AggregateRoot.java
+++ src/main/java/com/times132/ddd/common/domain/AggregateRoot.java
@@ -1,5 +1,15 @@
 package com.times132.ddd.common.domain;
 
-public class AggregateRoot {
+public class AggregateRoot<T> {
+    public T object;
+    protected String id;
 
+    protected AggregateRoot(T object, String id) {
+        this.object = object;
+        this.id = id;
+    }
+
+    public String getId() {
+        return this.id;
+    }
 }
